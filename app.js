3a6b7736b484ae5985b736ff79fe2e9a086ff466
Kendall Hayes <hayeskendall21@gmail.com>
2020-11-22 20:54:13 -0800

    Added MVP routes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const ipAddress = '127.0.0.1';
-const port = 8125;
+const port = 8126;
 var cors = require('cors');
 var cloudinary = require('cloudinary').v2;
 
@@ -21,6 +21,11 @@ app.use(cors());
 // parse application/json. Parse response/requests bodies to JSON
 //app.use(bodyParser.json());
 
+var folders = [];
+
+// parses and returns all request bodies
+app.use(express.json());
+
 app.get('/', async (req, res) => {
   // this will send only recent transactions
   const options = {
@@ -28,31 +33,97 @@ app.get('/', async (req, res) => {
   };
 
   cloudinary.api.resources(options, function (error, result) {
-    res.send(result.resources);
+    res.send(error === undefined ? result.resources : error);
   });
 
   // this will display folder names for organize images for users
   cloudinary.api.sub_folders('dna-images', (error, result) => {
+    folders = result.folders;
+
     console.log(error, result);
   });
 });
 
-app.get('/delete', async (req, res) => {
+app.get('/folders', async (req, res) => {
+  cloudinary.api.sub_folders('dna-images', (error, result) => {
+    // return all subfolders
+    folders = result.folders;
+    console.log(error, result);
+    res.send(error === undefined ? result : error);
+  });
+});
+
+app.get('/pick-folder', async (req, res) => {
+  // individual folder resource
+  console.log(await req.body);
+  cloudinary.api.resources(
+    {
+      type: 'upload',
+      prefix: `${req.body.pathName}/`,
+    },
+    function (error, result) {
+      res.send(error === undefined ? result : error);
+    }
+  );
+});
+
+app.get('/search', async (req, res) => {
+  // UI READY
+  // Resources by folder name
+  cloudinary.search
+    .expression(`folder:dna-images/${req.body.folderName}`)
+    .sort_by('public_id', 'desc')
+    .max_results(30)
+    .execute()
+    .then((result, error) => {
+      console.log(result);
+      res.send(result);
+    });
+});
+
+/* app.post('/upload', async(req,res)=>{
+  cloudinary.uploader.upload("gauges.jpg",
+  { responsive_breakpoints: 
+    { create_derived: true, 
+      bytes_step: 20000, 
+      min_width: 200, 
+      max_width: 1000 }}, 
+   function(error, result) {console.log(result, error); });
+}) */
+
+app.post('/delete', async (req, res) => {
   // ! There is a issue with locating images
-  const publicIDs = [
+  const rawPublicIDs = [
     'dna-images/albertsons-east-charleston/ductwork_ndnrvu.jpg',
+    'sample.jpg',
   ];
-  cloudinary.api.delete_resources(publicIDs, (error, result) => {
-    console.log(error, result);
-    res.send(result);
+
+  // logic for grabbing just the ID string with no file type ending
+  const formattedPublicIDs = rawPublicIDs.map((ID) => {
+    let formattedID = ID.slice(0, ID.length - 4);
+    return formattedID;
   });
+
+  cloudinary.api.delete_resources(
+    //UI READY
+    [req.body.publicIDs],
+    { type: 'upload' },
+    (error, result) => {
+      console.log(error, result);
+      res.send(error === undefined ? result : error);
+    }
+  );
 });
 
-app.get('/new-folder', async (req, res) => {
-  cloudinary.api.create_folder('dna-images/test', function (error, result) {
-    console.log(result);
-    res.send(result);
-  });
+app.post('/new-folder', async (req, res) => {
+  // UI READY
+  cloudinary.api.create_folder(
+    `dna-images/${req.body.new_folder_name}`,
+    function (error, result) {
+      console.log(result);
+      res.send(error === undefined ? result : error);
+    }
+  );
 });
 
 app.listen(port, ipAddress, () => {
---
+++ test.js
@@ -0,0 +1,15 @@
+const myArr = ['chicken.com', 'turkey.vid', 'tomatoes.jpg'];
+
+const newArr = myArr.map((item) => {
+  let formattedID = item.slice(0, item.length - 4);
+  return formattedID;
+});
+
+console.log(
+  'hello my name is maiya and i have the most gorilla grip coochie that you have ever had :P'
+);
+
+function mybiggestconfession(num1, num2) {
+  console.log(num1 + num2);
+}
+mybiggestconfession(1, 2);
