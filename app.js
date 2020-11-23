d85252bb16732f620aaeec1b62715655ced975e2
Kendall Hayes <hayeskendall21@gmail.com>
2020-11-23 12:24:30 -0800

    problem with the image search

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.get('/search', (req, res) => {
     .execute()
     .then((result, error) => {
       console.log(result);
-      res.send(result);
+      res.send(result.resources);
     });
 });
 
